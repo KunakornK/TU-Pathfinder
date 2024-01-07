8be7ef4f7166b56185ac450102b8e68f580e0a2a
KunakornK <104207522+KunakornK@users.noreply.github.com>
2024-01-07 20:52:26 +0700

    TU Pathfinder Mapbox Version 1.0

---
+++ MapBoxNavigation/app/src/main/java/com/example/mapboxnavigation/MainActivity.java
@@ -0,0 +1,347 @@
+package com.example.mapboxnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import static com.mapbox.maps.plugin.gestures.GesturesUtils.getGestures;
+import static com.mapbox.maps.plugin.locationcomponent.LocationComponentUtils.getLocationComponent;
+import static com.mapbox.maps.plugin.animation.CameraAnimationsUtils.getCamera;
+import static com.mapbox.maps.plugin.gestures.GesturesUtils.addOnMapClickListener;
+import static com.mapbox.navigation.base.extensions.RouteOptionsExtensions.applyDefaultNavigationOptions;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineResult;
+import com.mapbox.android.gestures.MoveGestureDetector;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.models.Bearing;
+import com.mapbox.api.directions.v5.models.RouteOptions;
+import com.mapbox.bindgen.Expected;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.EdgeInsets;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.extension.style.layers.properties.generated.TextAnchor;
+import com.mapbox.maps.plugin.LocationPuck2D;
+import com.mapbox.maps.plugin.animation.MapAnimationOptions;
+import com.mapbox.maps.plugin.annotation.AnnotationPlugin;
+import com.mapbox.maps.plugin.annotation.AnnotationPluginImplKt;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManagerKt;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions;
+import com.mapbox.maps.plugin.gestures.OnMapClickListener;
+import com.mapbox.maps.plugin.gestures.OnMoveListener;
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentConstants;
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener;
+import com.mapbox.maps.plugin.locationcomponent.generated.LocationComponentSettings;
+import com.mapbox.navigation.base.options.NavigationOptions;
+import com.mapbox.navigation.base.route.NavigationRoute;
+import com.mapbox.navigation.base.route.NavigationRouterCallback;
+import com.mapbox.navigation.base.route.RouterFailure;
+import com.mapbox.navigation.base.route.RouterOrigin;
+import com.mapbox.navigation.core.MapboxNavigation;
+import com.mapbox.navigation.core.directions.session.RoutesObserver;
+import com.mapbox.navigation.core.directions.session.RoutesUpdatedResult;
+import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp;
+import com.mapbox.navigation.core.trip.session.LocationMatcherResult;
+import com.mapbox.navigation.core.trip.session.LocationObserver;
+import com.mapbox.navigation.ui.base.util.MapboxNavigationConsumer;
+import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider;
+import com.mapbox.navigation.ui.maps.route.line.api.MapboxRouteLineApi;
+import com.mapbox.navigation.ui.maps.route.line.api.MapboxRouteLineView;
+import com.mapbox.navigation.ui.maps.route.line.model.MapboxRouteLineOptions;
+import com.mapbox.navigation.ui.maps.route.line.model.RouteLineError;
+import com.mapbox.navigation.ui.maps.route.line.model.RouteLineResources;
+import com.mapbox.navigation.ui.maps.route.line.model.RouteSetValue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    MapView mapView;
+
+    MaterialButton setRoute;
+    FloatingActionButton floatingActionButton;
+    private final NavigationLocationProvider navigationLocationProvider = new NavigationLocationProvider();
+    private MapboxRouteLineView routeLineView;
+    private MapboxRouteLineApi routeLineApi;
+
+    private final LocationObserver locationObserver = new LocationObserver() {
+        @Override
+        public void onNewRawLocation(@NonNull Location location) {
+
+        }
+
+        @Override
+        public void onNewLocationMatcherResult(@NonNull LocationMatcherResult locationMatcherResult) {
+            Location location = locationMatcherResult.getEnhancedLocation();
+            navigationLocationProvider.changePosition(location, locationMatcherResult.getKeyPoints(),null,null);
+            if(focusLocation){
+                updateCamera(Point.fromLngLat(location.getLongitude(),location.getLatitude()),(double)location.getBearing());
+            }
+        }
+    };
+
+    private final RoutesObserver routesObserver = new RoutesObserver() {
+        @Override
+        public void onRoutesChanged(@NonNull RoutesUpdatedResult routesUpdatedResult) {
+            routeLineApi.setNavigationRoutes(routesUpdatedResult.getNavigationRoutes(), new MapboxNavigationConsumer<Expected<RouteLineError, RouteSetValue>>() {
+                @Override
+                public void accept(Expected<RouteLineError, RouteSetValue> routeLineErrorRouteSetValueExpected) {
+                    Style style = mapView.getMapboxMap().getStyle();
+                    if(style != null){
+                        routeLineView.renderRouteDrawData(style,routeLineErrorRouteSetValueExpected);
+                    }
+                }
+            });
+        }
+    };
+    boolean focusLocation = true;
+    private MapboxNavigation mapboxNavigation;
+    private void updateCamera(Point point, Double bearing) {
+        MapAnimationOptions animationOptions = new MapAnimationOptions.Builder().duration(1500L).build();
+        CameraOptions cameraOptions = new CameraOptions.Builder().center(point).zoom(18.0).bearing(bearing).pitch(45.0)
+                .padding(new EdgeInsets(1000.0, 0.0, 0.0, 0.0)).build();
+
+        getCamera(mapView).easeTo(cameraOptions, animationOptions);
+    }
+
+
+
+    private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean result) {
+            if(result){
+                Toast.makeText(MainActivity.this,"Permission Granted",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(MainActivity.this,"Permission Not Granted Please Give permission And Restart",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    });
+
+    private final OnIndicatorBearingChangedListener onIndicatorBearingChangedListener = new OnIndicatorBearingChangedListener() {
+        @Override
+        public void onIndicatorBearingChanged(double v) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().bearing(v).build());
+        }
+    };
+
+    private final OnIndicatorPositionChangedListener onIndicatorPositionChangedListener = new OnIndicatorPositionChangedListener() {
+        @Override
+        public void onIndicatorPositionChanged(@NonNull Point point) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().center(point).zoom(20.0).build());
+            getGestures(mapView).setFocalPoint(mapView.getMapboxMap().pixelForCoordinate(point));
+        }
+    };
+
+    private final OnMoveListener onMoveListener = new OnMoveListener() {
+        @Override
+        public void onMoveBegin(@NonNull MoveGestureDetector moveGestureDetector) {
+            getLocationComponent(mapView).removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+            getLocationComponent(mapView).removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+            floatingActionButton.show();
+        }
+
+        @Override
+        public boolean onMove(@NonNull MoveGestureDetector moveGestureDetector) {
+            return false;
+        }
+
+        @Override
+        public void onMoveEnd(@NonNull MoveGestureDetector moveGestureDetector) {
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapView);
+        floatingActionButton = findViewById(R.id.focusLocation);
+        floatingActionButton.hide();
+        setRoute = findViewById(R.id.setRoute);
+
+        MapboxRouteLineOptions options = new MapboxRouteLineOptions.Builder(this).withRouteLineResources(new RouteLineResources.Builder().build())
+                .withRouteLineBelowLayerId(LocationComponentConstants.LOCATION_INDICATOR_LAYER).build();
+        routeLineView = new MapboxRouteLineView(options);
+        routeLineApi = new MapboxRouteLineApi(options);
+
+        NavigationOptions navigationOptions = new NavigationOptions.Builder(this).accessToken(getString(R.string.mapbox_access_token)).build();
+        MapboxNavigationApp.setup(navigationOptions);
+        mapboxNavigation = new MapboxNavigation(navigationOptions);
+
+        mapboxNavigation.registerRoutesObserver(routesObserver);
+        mapboxNavigation.registerLocationObserver(locationObserver);
+
+
+        activityResultLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        activityResultLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        LocationComponentPlugin locationComponentPlugin = getLocationComponent(mapView);
+        getGestures(mapView).addOnMoveListener(onMoveListener);
+
+        setRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Please select a location in map", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                mapView.getMapboxMap().setCamera(new CameraOptions.Builder().build());
+                LocationComponentPlugin locationComponentPlugin = getLocationComponent(mapView);
+                locationComponentPlugin.setEnabled(true);
+                LocationPuck2D locationPuck2D = new LocationPuck2D();
+                locationPuck2D.setBearingImage(AppCompatResources.getDrawable(MainActivity.this,R.drawable.baseline_accessibility_24));
+                locationComponentPlugin.setLocationPuck(locationPuck2D);
+                locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+                getGestures(mapView).addOnMoveListener(onMoveListener);
+                locationComponentPlugin.updateSettings(new Function1<LocationComponentSettings, Unit>() {
+                    @Override
+                    public Unit invoke(LocationComponentSettings locationComponentSettings) {
+                        locationComponentSettings.setEnabled(true);
+                        locationComponentSettings.setPulsingEnabled(true);
+                        return null;
+                    }
+                });
+
+
+
+
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.location_pin);
+                Bitmap resized = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+                AnnotationPlugin annotationPlugin = AnnotationPluginImplKt.getAnnotations(mapView);
+                PointAnnotationManager pointAnnotationManager = PointAnnotationManagerKt.createPointAnnotationManager(annotationPlugin, mapView);
+                addOnMapClickListener(mapView.getMapboxMap(), new OnMapClickListener() {
+                    @Override
+                    public boolean onMapClick(@NonNull Point point) {
+                        pointAnnotationManager.deleteAll();
+                        PointAnnotationOptions pointAnnotationOptions = new PointAnnotationOptions().withTextAnchor(TextAnchor.CENTER).withIconImage(resized).withPoint(point);
+                        pointAnnotationManager.create(pointAnnotationOptions);
+
+                        setRoute.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                fineRoute(point);
+                            }
+                        });
+                        return true;
+                    }
+                });
+                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+                        locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                        getGestures(mapView).addOnMoveListener(onMoveListener);
+                        floatingActionButton.hide();
+                    }
+                });
+
+
+
+
+
+            }
+        });
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void fineRoute(Point point){
+        LocationEngine locationEngine = LocationEngineProvider.getBestLocationEngine(MainActivity.this);
+        locationEngine.getLastLocation(new LocationEngineCallback<LocationEngineResult>() {
+            @Override
+            public void onSuccess(LocationEngineResult result) {
+                Location location = result.getLastLocation();
+                setRoute.setEnabled(false);
+                setRoute.setText("Finding Route..");
+                RouteOptions.Builder builder = RouteOptions.builder();
+                Point origin = Point.fromLngLat(Objects.requireNonNull(location).getLongitude(), location.getLatitude());
+                builder.coordinatesList(Arrays.asList(origin, point));
+                builder.alternatives(false);
+                builder.profile(DirectionsCriteria.PROFILE_WALKING);
+                builder.bearingsList(Arrays.asList(Bearing.builder().angle(location.getBearing()).degrees(45.0).build(),null));
+
+                applyDefaultNavigationOptions(builder).profile(DirectionsCriteria.PROFILE_WALKING);
+                //builder.applyDefaultNavigationOptions();
+
+                mapboxNavigation.requestRoutes(builder.build(), new NavigationRouterCallback() {
+                    @Override
+                    public void onRoutesReady(@NonNull List<NavigationRoute> list, @NonNull RouterOrigin routerOrigin) {
+                        mapboxNavigation.setNavigationRoutes(list);
+                        floatingActionButton.performClick();
+                        setRoute.setEnabled(true);
+                        setRoute.setText("See Route");
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull List<RouterFailure> list, @NonNull RouteOptions routeOptions) {
+                        setRoute.setEnabled(true);
+                        setRoute.setText("See Route");
+                        Toast.makeText(MainActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCanceled(@NonNull RouteOptions routeOptions, @NonNull RouterOrigin routerOrigin) {
+
+                    }
+                });
+
+
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapboxNavigation.onDestroy();
+        mapboxNavigation.unregisterRoutesObserver(routesObserver);
+        mapboxNavigation.unregisterLocationObserver(locationObserver);
+    }
+}
